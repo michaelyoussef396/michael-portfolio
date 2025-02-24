0bbd6f68a759f4ca886d8397e3f1ce0df5540868
michael youssef <michaelyoussef396@gmail.com>
2025-02-24 15:03:33 +1100

    created about us section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,6 +6,7 @@ import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Certificate from "@/sections/Certificate";
 import Footer from "@/components/Footer";
 import Hero3D from "@/components/Hero3D";
+import About from "@/sections/About";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero3D />
+        <About />
         <Certificate />
         <Footer />
       </div>
