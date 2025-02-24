7f2adfd3b739f9386c63c59ef16bec0a80381c9a
michael youssef <michaelyoussef396@gmail.com>
2025-02-24 20:07:00 +1100

    created hero porfolio section

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,23 +1,23 @@
 "use client";
 
 import { navItems } from "@/data";
-
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Footer from "@/components/Footer";
 import FAQ from "@/components/FAQ";
 import { EmailCTA } from "@/components/EmailCTA";
+import HeroPortfolio from "@/sections/HeroPortfolio";
 
-const AboutMe = () => {
+const Portfolio = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
-        <FAQ />
-        <EmailCTA />
-        <Footer />
-      </div>
+    <main className="overflow-hidden bg-black-100 w-full h-full">
+      {/* If you want it truly full screen, remove max-width constraints */}
+      <FloatingNav navItems={navItems} />
+      <HeroPortfolio />
+      <FAQ />
+      <EmailCTA />
+      <Footer />
     </main>
   );
 };
 
-export default AboutMe;
+export default Portfolio;
