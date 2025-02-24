db1646277cb12245dec4676da13d2b0eedcc1d0e
michael youssef <michaelyoussef396@gmail.com>
2025-02-24 14:17:49 +1100

    add Cube.tsx component with GSAP-driven rotation and matcap texture

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,12 +5,14 @@ import { navItems } from "@/data";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Certificate from "@/sections/Certificate";
 import Footer from "@/components/Footer";
+import Hero3D from "@/components/Hero3D";
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
+        <Hero3D />
         <Certificate />
         <Footer />
       </div>
