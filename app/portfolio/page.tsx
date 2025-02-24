e4c2b8d96950a2357310090ac0608664832316bb
michael youssef <michaelyoussef396@gmail.com>
2025-02-24 19:48:23 +1100

    created porfolio pafe layoput

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,12 +3,7 @@
 import { navItems } from "@/data";
 
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import Certificate from "@/sections/Certificate";
 import Footer from "@/components/Footer";
-import Hero3D from "@/components/Hero3D";
-import About from "@/sections/About";
-import Tech from "@/sections/Tech";
-import Experience from "@/sections/Experience";
 import FAQ from "@/components/FAQ";
 import { EmailCTA } from "@/components/EmailCTA";
 
@@ -17,11 +12,6 @@ const AboutMe = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
-        <Hero3D />
-        <About />
-        <Experience />
-        <Certificate />
-        <Tech />
         <FAQ />
         <EmailCTA />
         <Footer />
