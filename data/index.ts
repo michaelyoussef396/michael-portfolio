57e02295f1015db0dba09f060c234a45d90f4c87
michaelyoussef396 <mihcaelyoussef396@gmail.com>
2025-02-25 22:41:35 +1100

    changed inside loop

--- data/index.ts
+++ data/index.ts
@@ -52,12 +52,12 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "Currently running Sole Media, the marketing agency i co-founded",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/b5.svg",
+    img: "/insideloop.png",
     spareImg: "/grid.svg",
   },
   {
